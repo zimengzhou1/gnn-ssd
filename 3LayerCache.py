4edc9c8ffc32f52979fa3b46edebd03889294c8b
Zimeng Zhou <zimeng.zhou@gmail.com>
2023-03-05 23:44:46 +0000

    Add caching test script

--- 3LayerCache.py
+++ 3LayerCache.py
@@ -3,20 +3,24 @@ import os.path as osp
 import os
 import sys
 import torch
-from overflowDataset import OpenFlowDataset
+from overflowDataset import OverFlowDataset
+from torch_geometric.datasets import JODIEDataset
 from tqdm import tqdm
 from neighbor_sampler import NeighborSampler
 import argparse
 import scipy
 import numpy as np
+from torch_geometric.data import Data
+from torch_geometric.utils import to_undirected
 
 
 parser = argparse.ArgumentParser('Cache testing')
-parser.add_argument('--subset', type=float, default=1, help='percentage of data to sample from')
+parser.add_argument('--subsetPerc', type=float, default=1, help='percentage of data to sample from')
 parser.add_argument('--CPUCachePerc', type=int, default=20, help='CPU cache percentage of data')
-parser.add_argument('--LRUOnly', default=True, action='store_true')
-parser.add_argument('--no-LRUOnly', dest='LRUOnly', action='store_false')
+# parser.add_argument('--LRUOnly', default=True, action='store_true')
+# parser.add_argument('--no-LRUOnly', dest='LRUOnly', action='store_false')
 parser.add_argument('--sizes', type=str, default='10,10')
+parser.add_argument('--dataset', type=str, default='reddit')
 
 try:
   args = parser.parse_args()
@@ -24,22 +28,47 @@ except:
   parser.print_help()
   sys.exit(0)
 
-print("Using subset of: ", args.subset, "%, CPU cache of: ", args.CPUCachePerc, "% ", "LRU only: ", args.LRUOnly)
+print("Using ", args.subsetPerc, "% of data, CPU cache of: ", args.CPUCachePerc, "% ")
 
 sizes = [int(size) for size in args.sizes.split(',')]
 
 __file__ = os.path.abspath('')
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'OpenFlow')
 
-print("Loading dataset...")
-dataset = OpenFlowDataset(path)
-data = dataset[0]
+dataName = args.dataset
+print("Loading dataset ", dataName, "...")
+
+if dataName == 'overflow':
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'overflow')
+    dataset = OverFlowDataset(path)
+    data = dataset[0]
+    orig_edge_index = data.edge_index
+elif dataName == 'taobao':
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'taobao', 'taobao.pt')
+    data = torch.load(path)
+    orig_edge_index = data.edge_index
+    data.edge_index = to_undirected(data.edge_index)
+elif dataName == 'reddit':
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'JODIE')
+    dataset = JODIEDataset(path, name='reddit')
+    data_orig = dataset[0]
+    data = Data(x=data_orig.msg, edge_index=torch.stack([data_orig.src, data_orig.dst], dim=0), edge_attr=data_orig.t)
+    orig_edge_index = data.edge_index
+    data.edge_index = to_undirected(data.edge_index)
+elif dataName == 'wiki':
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'JODIE')
+    dataset = JODIEDataset(path, name='wikipedia')
+    data_orig = dataset[0]
+    data = Data(x=data_orig.msg, edge_index=torch.stack([data_orig.src, data_orig.dst], dim=0), edge_attr=data_orig.t)
+    orig_edge_index = data.edge_index
+    data.edge_index = to_undirected(data.edge_index)
  
 class LRUCache:
     def __init__(self, capacity: int):
         self.cache = OrderedDict()
         self.capacity = capacity
         self.stats = []
+        self.capacityReached = False
  
     def get(self, key: int) -> int:
         if key not in self.cache:
@@ -55,73 +84,79 @@ class LRUCache:
         self.cache.move_to_end(key)
         if len(self.cache) > self.capacity:
             self.cache.popitem(last = False)
+            if not self.capacityReached:
+                self.capacityReached = True
+                return 1
+        return 0
 
+subset = int(orig_edge_index[0].numel() / (100/args.subsetPerc))
 
-subset = int(data.num_nodes / (100/args.subset))
+n1 = torch.unique(orig_edge_index[0])
+n2 = torch.unique(orig_edge_index[1])
+total_nodes = torch.unique(torch.cat((n1,n2))).numel()
 
 # Assume CPU cache is 20% of data
-CPUCacheNum = int(subset / (100/args.CPUCachePerc))
+CPUCacheNum = int(total_nodes / (100/args.CPUCachePerc))
 
 # Assume GPU cache is 0.25% of data
-GPUCacheNum = int(subset / 200)
+GPUCacheNum = int(total_nodes / 200)
 node_ids = torch.flatten(data.edge_index.t())
+nodes_to_sample = node_ids[len(node_ids) - subset*2:]
+nodes_to_sample_unique_num = torch.unique(nodes_to_sample).numel()
+# print("Total number of unique nodes in dataset: ", total_nodes)
+# print("Number of unique nodes in edges we sample: ", nodes_to_sample_unique_num)
+# print("Number of total edges sampled: ", subset)
 
 
+CPUCacheLRU  = LRUCache(CPUCacheNum)
+CPUCacheStatic = LRUCache(CPUCacheNum)
 GPUCache = LRUCache(GPUCacheNum)
 
 # We sample from end of data
-temp_loader = NeighborSampler(data.edge_index, sizes=sizes, node_idx=node_ids[len(node_ids) - subset*2:], batch_size=2)
+print("Initializing sampler")
+loader = NeighborSampler(data.edge_index, sizes=sizes, node_idx=nodes_to_sample, batch_size=2)
 
-def runLRU():
-  CPUCache  = LRUCache(CPUCacheNum)
+# Get out neighbor statistics
+coo = orig_edge_index.numpy()
+v = np.ones_like(coo[0])
+coo = scipy.sparse.coo_matrix((v, (coo[0], coo[1])), shape=(orig_edge_index[0].numel(), orig_edge_index[0].numel()))
+csc = coo.tocsc()
+csr = coo.tocsr()
 
+csc_indptr_tensor = torch.from_numpy(csc.indptr.astype(np.int64))
+csr_indptr_tensor = torch.from_numpy(csr.indptr.astype(np.int64))
+out_num_neighbors = csr_indptr_tensor[1:] - csr_indptr_tensor[:-1]
+in_num_neighbors = (csc_indptr_tensor[1:] - csc_indptr_tensor[:-1])
+sorted_vals_out, indices_out = torch.sort(out_num_neighbors, descending=True)
+sorted_vals_in, indices_in = torch.sort(in_num_neighbors, descending=True)
+
+# Populate static cache with highest out degree nodes
+for i in range(CPUCacheNum):
+    val = int(indices_out[i])
+    CPUCacheStatic.put(val, val)
+
+# Run LRU and static Cache
+def run(CPUCacheLRU, CPUCacheStatic):
+  numEdgeProcessed = 0
   pbar = tqdm(total=subset*2)
-  for batch_size, ids, adjs in temp_loader:
+  for batch_size, ids, adjs in loader:
     for i in ids:
       i = int(i)
-      val = CPUCache.get(i)
+      val = CPUCacheLRU.get(i)
+      CPUCacheStatic.get(i)
       if (val == -1):
         # Fetch from SSD
-        CPUCache.put(i,i)
+        putVal = CPUCacheLRU.put(i,i)
+        if putVal == 1:
+          print(f"After {numEdgeProcessed} edges ({numEdgeProcessed*100/subset:.2f}%), cache capacity reached")
+    numEdgeProcessed += 1
     pbar.update(batch_size)
   pbar.close()
 
-  t = torch.tensor(CPUCache.stats)
-  torch.save(t, "LRU_subset_" + str(args.subset) + "Ccache_" + str(args.CPUCachePerc) + '.pt')
+  t1 = torch.tensor(CPUCacheLRU.stats)
+  torch.save(t1, "cache_data/LRU_" + dataName + "_subset_" + str(args.subsetPerc) + "Cache_" + str(args.CPUCachePerc) + "Size_" + args.sizes.replace(",","_") + '.pt')
+  t2 = torch.tensor(CPUCacheStatic.stats)
+  torch.save(t2, "cache_data/static_" + dataName + "_subset_" + str(args.subsetPerc) + "Cache_" + str(args.CPUCachePerc) + "Size_" + args.sizes.replace(",","_") + '.pt')
 
-def runStatic():
-  CPUCache  = LRUCache(CPUCacheNum)
-
-  coo = data.edge_index.numpy()
-  v = np.ones_like(coo[0])
-  coo = scipy.sparse.coo_matrix((v, (coo[0], coo[1])), shape=(data.num_nodes, data.num_nodes))
-  csc = coo.tocsc()
-  csr = coo.tocsr()
-
-  csc_indptr_tensor = torch.from_numpy(csc.indptr.astype(np.int64))
-  csr_indptr_tensor = torch.from_numpy(csr.indptr.astype(np.int64))
-  out_num_neighbors = csr_indptr_tensor[1:] - csr_indptr_tensor[:-1]
-  in_num_neighbors = (csc_indptr_tensor[1:] - csc_indptr_tensor[:-1])
-
-  sorted_vals_out, indices_out = torch.sort(out_num_neighbors, descending=True)
-
-  # Populate cache with highest out degree nodes
-  for i in range(CPUCacheNum):
-    val = int(indices_out[i])
-    CPUCache.put(val, val)
-  
-  pbar = tqdm(total=subset*2)
-  for batch_size, ids, adjs in temp_loader:
-    for i in ids:
-      i = int(i)
-      val = CPUCache.get(i)
-    pbar.update(batch_size)
-  pbar.close()
-
-  t = torch.tensor(CPUCache.stats)
-  torch.save(t, "static_subset_" + str(args.subset) + "Ccache_" + str(args.CPUCachePerc) + '.pt')
-
-if args.LRUOnly:
-   runLRU()
-else:
-   runStatic()
+print("Running requests...")
+run(CPUCacheLRU, CPUCacheStatic)
--- cacheHitGraph.py
+++ cacheHitGraph.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import argparse
 import sys
 from tqdm import tqdm
+import numpy as np
 
 parser = argparse.ArgumentParser('Cache Graphing')
 parser.add_argument('--path', type=str)
@@ -15,34 +16,25 @@ except:
 
 print("Using path: ", args.path[:-3])
 
-
-def sum_intervals(vals, interval_size):
-    num_intervals = len(vals) // interval_size
-    interval_sums = []
-    pbar = tqdm(total=num_intervals)
-    for i in range(num_intervals):
-        start_index = i * interval_size
-        end_index = start_index + interval_size
-        interval = vals[start_index:end_index]
-        interval_sum = sum(interval) / interval_size
-        interval_sums.append(interval_sum)
-        pbar.update(1)
-    pbar.close()
-    return interval_sums
-
 def getHitRate(stats):
     return sum(stats)/len(stats)
 
-t = torch.load(args.path)
-t = list(t)
+def sum_intv_new(vals, interval_size):
+  return np.nanmean(np.pad(vals.astype(float), (0, interval_size - vals.size%interval_size), mode='constant', constant_values=np.NaN).reshape(-1, interval_size), axis=1)
+
+t1 = torch.load(args.path[:11] + "LRU_" + args.path[11:]).numpy()
+t2 = torch.load(args.path[:11] + "static_" + args.path[11:]).numpy()
 
 interval_size = 20000
-intervals_dynamic = sum_intervals(t, interval_size)
-print(getHitRate(intervals_dynamic))
+LRU_intervals = sum_intv_new(t1, interval_size)
+static_intervals = sum_intv_new(t2, interval_size)
+print("static hit rate: ", getHitRate(static_intervals))
+print("LRU hit rate: ", getHitRate(LRU_intervals))
 
-
-plt.plot(intervals_dynamic)
+plt.plot(static_intervals, label='static')
+plt.plot(LRU_intervals, label='LRU')
 plt.title('Average cache hit per ' + str(interval_size) + ' requests')
 plt.ylabel('Cache hit percentage')
 plt.xlabel('Iterations')
+plt.legend()
 plt.savefig(args.path[:-3] + '.png')
