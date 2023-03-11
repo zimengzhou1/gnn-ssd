c3e29194f5b28515d761c2117e8c65173f60d128
Zimeng Zhou <zimeng.zhou@gmail.com>
2023-03-11 00:14:55 +0000

    Temporal sampling and static fix

--- 3LayerCache.py
+++ 3LayerCache.py
@@ -7,6 +7,7 @@ from overflowDataset import OverFlowDataset
 from torch_geometric.datasets import JODIEDataset
 from tqdm import tqdm
 from neighbor_sampler import NeighborSampler
+from torch_geometric.loader import LinkNeighborLoader
 import argparse
 import scipy
 import numpy as np
@@ -67,6 +68,10 @@ n1 = torch.unique(orig_edge_index[0])
 n2 = torch.unique(orig_edge_index[1])
 total_nodes = torch.unique(torch.cat((n1,n2))).numel()
 
+data.n_id = torch.arange(total_nodes)
+data.ts_n = torch.zeros(total_nodes).to(dtype=torch.long)
+data.x = data.n_id
+
 # Assume CPU cache is 20% of data
 CPUCacheNum = int(total_nodes / (100/args.CPUCachePerc))
 metadata['cacheSize'] = CPUCacheNum
@@ -87,10 +92,25 @@ GPUCache = LRUCache(GPUCacheNum)
 
 # We sample from end of data
 #print("Initializing sampler")
-loader = NeighborSampler(data.edge_index, sizes=sizes, node_idx=nodes_to_sample, batch_size=2)
+# loader = NeighborSampler(data.edge_index, sizes=sizes, node_idx=nodes_to_sample, batch_size=2)
+# print(orig_edge_index[0].numel()- subset)
+sample_idx = orig_edge_index[0].numel() - subset
+sampled_edges = torch.stack([orig_edge_index[0][sample_idx:], orig_edge_index[1][sample_idx:]])
+neighbor_loader = LinkNeighborLoader(data, num_neighbors=sizes, edge_label_index=sampled_edges, edge_label_time=data.edge_attr[sample_idx:], time_attr='ts_n')
 
 # Get out neighbor statistics
-coo = data.edge_index.numpy()
+# Take first 80% of data to find top out-degree neighbors
+sample_num = orig_edge_index[0].numel() - int(orig_edge_index[0].numel() / (100/80))
+sampled_edges = torch.stack([orig_edge_index[0][:sample_num], orig_edge_index[1][:sample_num]])
+# print(sampled_edges)
+sampled_edges = to_undirected(sampled_edges)
+# Find number of unique nodes in 80% of data
+#n1 = torch.unique(sampled_edges[0])
+#n2 = torch.unique(sampled_edges[1])
+#total_nodes = torch.unique(torch.cat((n1,n2))).numel()
+
+coo = sampled_edges.numpy()
+#coo = data.edge_index.numpy()
 v = np.ones_like(coo[0])
 coo = scipy.sparse.coo_matrix((v, (coo[0], coo[1])), shape=(total_nodes, total_nodes))
 csc = coo.tocsc()
@@ -102,6 +122,10 @@ out_num_neighbors = csr_indptr_tensor[1:] - csr_indptr_tensor[:-1]
 in_num_neighbors = (csc_indptr_tensor[1:] - csc_indptr_tensor[:-1])
 sorted_vals_out, indices_out = torch.sort(out_num_neighbors, descending=True)
 sorted_vals_in, indices_in = torch.sort(in_num_neighbors, descending=True)
+# print(len([i for i in list(sorted_vals_out) if int(i)!=0]))
+# print(len(sorted_vals_out))
+# print(sorted_vals_out[:20])
+# print(sorted_vals_out[len(sorted_vals_out)-10:])
 
 # Populate static cache with highest out degree nodes
 for i in range(CPUCacheNum):
@@ -114,9 +138,15 @@ def getHitRate(stats):
 # Run LRU and static Cache
 def run(CPUCacheLRU, CPUCacheStatic, CPUCacheLFU, CPUCacheARC):
   numEdgeProcessed = 0
-  pbar = tqdm(total=subset*2)
-  for batch_size, ids, adjs in loader:
-    for i in ids:
+  # pbar = tqdm(total=subset*2)
+  pbar = tqdm(total=subset)
+  # for batch_size, ids, adjs in loader:
+  for data in neighbor_loader:
+    # for i in ids:
+    
+    nodes = data.n_id
+    nodes = torch.unique(nodes)
+    for i in nodes:
       i = int(i)
       CPUCacheStatic.get(i)
       LRUval = CPUCacheLRU.get(i)
@@ -132,7 +162,8 @@ def run(CPUCacheLRU, CPUCacheStatic, CPUCacheLFU, CPUCacheARC):
           metadata['capacityReached'] = numEdgeProcessed
           #print(f"After {numEdgeProcessed} edges ({numEdgeProcessed*100/subset:.2f}%), cache capacity reached")
     numEdgeProcessed += 1
-    pbar.update(batch_size)
+    # pbar.update(batch_size)
+    pbar.update(1)
   pbar.close()
 
   t1 = torch.tensor(CPUCacheLRU.stats)
